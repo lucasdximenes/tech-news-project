d5105c92dc09e70c6916e5f697f22c8fff464935
Lucas Ximenes <lucasdximenes@gmail.com>
2023-04-19 23:09:54 -0300

    feat: :rocket: implement `top_5_categories` function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,16 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    # Seu código
+    all_news_categories = [news["category"] for news in find_news()]
+    categories = set(all_news_categories)
+    categories_count = {
+        category: all_news_categories.count(category)
+        for category in categories
+    }
+    categories_count = sorted(
+        categories_count.items(), key=lambda x: (-x[1], x[0])
+    )
+    return [category[0] for category in categories_count[:5]]
