93a7f8395400e922c1a81217207f04c5eabcb7e9
Lucas Ximenes <lucasdximenes@gmail.com>
2023-04-19 21:34:40 -0300

    feat: :rocket: implement `scrape_news` function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,16 +2,13 @@ import requests
 from parsel import Selector
 from time import sleep
 
-BASE_URL = "https://blog.betrybe.com/"
-
 
 # Requisito 1
 def fetch(url):
     try:
         response = requests.get(
-            BASE_URL, headers={"User-Agent": "Fake user-agent"}, timeout=3
+            url, headers={"User-Agent": "Fake user-agent"}, timeout=3
         )
-        print(response)
         response.raise_for_status()
         return response.text
     except requests.exceptions.HTTPError:
@@ -38,7 +35,30 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    html_body = Selector(text=html_content)
+    news_url = html_body.css("link[rel=canonical]::attr(href)").get()
+    news_title = html_body.css("h1.entry-title::text").get().strip()
+    news_timestamp = html_body.css(".meta-date::text").re_first(
+        r"\d{2}/\d{2}/\d{4}"
+    )
+    news_writer = html_body.css(".author a::text").get()
+    news_reading_time = int(
+        html_body.css(".meta-reading-time::text").re_first(r"\d+")
+    )
+    news_summary = "".join(
+        html_body.css(".entry-content > p:first-of-type *::text").getall()
+    ).strip()
+    news_category = html_body.css(".category-style span.label::text").get()
+
+    return {
+        "url": news_url,
+        "title": news_title,
+        "timestamp": news_timestamp,
+        "writer": news_writer,
+        "reading_time": news_reading_time,
+        "summary": news_summary,
+        "category": news_category,
+    }
 
 
 # Requisito 5
