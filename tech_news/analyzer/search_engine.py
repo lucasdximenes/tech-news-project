47e48e61cd07f41f756e7845d6fe5828095f06b1
Lucas Ximenes <lucasdximenes@gmail.com>
2023-04-19 22:51:29 -0300

    feat: :rocket: implement `search_by_title` function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,12 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    return [
+        (news["title"], news["url"])
+        for news in search_news({"title": {"$regex": title, "$options": "i"}})
+    ]
 
 
 # Requisito 8
